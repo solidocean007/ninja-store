ce8e5c68b99320a29726f88e01a691eb93ebcf37
solidocean007 <clintonwilliams007@gmail.com>
2023-04-03 20:21:55 -0400

    Work on login sign up form

---
+++ src/Components/InputBase.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './InputBase.css';
+
+const InputBase = () => (
+  <label htmlFor="">
+    <input className="input-root" type="text" />
+
+  </label>
+) 
+
+export default InputBase;
---
+++ src/Components/SignUp_Login.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import InputBase from './InputBase'
+
+function SignUp_Login() {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+    userName: '',
+    zip: '',
+    cartItems: '',
+    totalBill: '',
+  })
+
+  const handleCreateAccount = (event) => {
+    event.preventDefault();
+    // const newAccount;
+    }
+  
+  const handleInputData = (event) => {
+    const { name, value } = event.target;
+    setInputs((oldValues) => ({...oldValues, [name]: value}));
+  };
+
+  // handleBlur = ({ target: { name, value }}) => this.handleValidations(name, value); 
+
+  const inputData = [
+    { label: 'Your email address', name: 'email', type: 'text', error: 'emailError'},
+    { label: 'Create a password', name: 'password', type: 'text', error: 'passwordError'},
+    { label: 'Confirm your password', name: 'passwordConfirm', type: 'text', error: 'confirmPasswordError'},
+    { label: 'Your Name', name: 'userName', type: 'text', error: 'nameError'},
+    { label: 'Zip code', name: 'zip', type: 'text', error: 'zipError'},
+  ]
+
+  return (
+    <div className='logInOptions'>
+      <button>SIGN IN</button>
+      <button>CREATE ACCOUNT</button>
+      <form onSubmit={handleCreateAccount}>
+        { inputData.length
+          ? inputData.map((item) => (
+            <InputBase
+              key={item.name}
+              placeholder={item.label}
+              type={item.type}
+              onChange={handleInputData}
+              name={item.name}
+              // onBlur={handleBlur}
+              // error={error}
+              // errorMessage={
+              //   (error
+              //     && error[item.error]
+              //     && error[item.error].length > 1)
+              //     ? error[item.error]
+              //     : null
+              //   }
+                />
+          ))
+          : null
+        }
+        <div className='btn-wrapper'>
+          <input type="submit" value='Save' />
+        </div>
+        <div className='btn-wrapper'>
+          <input type="submit" value='Facebook' />
+        </div>
+      </form>
+
+    </div>
+  )
+};
+
+export default SignUp_Login
